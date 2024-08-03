9194c5d70cca4f1eaa591153d2a25037b8140ab9
dudko <f.j.kowalik@gmail.com>
2024-08-03 14:34:57 +0200

    port tooltips

--- common/src/main/java/com/dudko/blazinghot/BlazingHot.java
+++ common/src/main/java/com/dudko/blazinghot/BlazingHot.java
@@ -8,6 +8,10 @@ import com.dudko.blazinghot.data.recipe.BlazingSequencedAssemblyRecipeGen;
 import com.dudko.blazinghot.registry.*;
 import com.simibubi.create.Create;
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipHelper;
+import com.simibubi.create.foundation.item.TooltipModifier;
 import com.tterrag.registrate.providers.ProviderType;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.data.DataGenerator;
@@ -21,6 +25,11 @@ public class BlazingHot {
     public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
     private static final CreateRegistrate REGISTRATE = CreateRegistrate.create(BlazingHot.ID);
+
+    static {
+        REGISTRATE.setTooltipModifierFactory(item -> new ItemDescription.Modifier(item,
+                TooltipHelper.Palette.STANDARD_CREATE).andThen(TooltipModifier.mapNull(KineticStats.create(item))));
+    }
 
     public static void init() {
         LOGGER.info("Create mod addon {} initializing! Create version: {}", NAME, Create.VERSION);
