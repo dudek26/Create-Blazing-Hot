8d06b8389fe8443aec80004663bfa15686266bcc
dudko <f.j.kowalik@gmail.com>
2024-08-08 00:54:16 +0200

    ported jei compat to forge

---
+++ forge/src/main/java/com/dudko/blazinghot/compat/jei/forge/BlazeMixingCategory.java
@@ -0,0 +1,88 @@
+package com.dudko.blazinghot.compat.jei.forge;
+
+import com.dudko.blazinghot.content.kinetics.blaze_mixer.BlazeMixingRecipe;
+import com.dudko.blazinghot.registry.BlazingTags;
+import com.dudko.blazinghot.util.FluidUtil;
+import com.simibubi.create.compat.jei.category.BasinCategory;
+import com.simibubi.create.compat.jei.category.animations.AnimatedBlazeBurner;
+import com.simibubi.create.content.processing.basin.BasinRecipe;
+import com.simibubi.create.content.processing.recipe.HeatCondition;
+import com.simibubi.create.foundation.fluid.FluidIngredient;
+import mezz.jei.api.forge.ForgeTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.fluids.FluidStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlazeMixingCategory extends BasinCategory {
+
+    private final AnimatedBlazeMixer mixer = new AnimatedBlazeMixer();
+    private final AnimatedBlazeBurner heater = new AnimatedBlazeBurner();
+    MixingType type;
+
+    protected enum MixingType {
+        MIXING,
+        AUTO_SHAPELESS,
+        AUTO_BREWING
+    }
+
+    public static BlazeMixingCategory standard(Info<BasinRecipe> info) {
+        return new BlazeMixingCategory(info, MixingType.MIXING);
+    }
+
+    public static BlazeMixingCategory autoShapeless(Info<BasinRecipe> info) {
+        return new BlazeMixingCategory(info, MixingType.AUTO_SHAPELESS);
+    }
+
+    public static BlazeMixingCategory autoBrewing(Info<BasinRecipe> info) {
+        return new BlazeMixingCategory(info, MixingType.AUTO_BREWING);
+    }
+
+    protected BlazeMixingCategory(Info<BasinRecipe> info, MixingType type) {
+        super(info, type != MixingType.AUTO_SHAPELESS);
+        this.type = type;
+    }
+
+    @Override
+    public void setRecipe(@NotNull IRecipeLayoutBuilder builder, @NotNull BasinRecipe recipe, @NotNull IFocusGroup focuses) {
+        super.setRecipe(builder, recipe, focuses);
+        FluidIngredient fuelFluid;
+        if (recipe instanceof BlazeMixingRecipe bmRecipe) fuelFluid = bmRecipe.getFuelFluid();
+        else {
+            int
+                    calculatedCost =
+                    (int) FluidUtil.platformedAmount(BlazeMixingRecipe.durationToFuelCost(recipe.getProcessingDuration()));
+            fuelFluid = FluidIngredient.fromTag(BlazingTags.Fluids.BLAZE_MIXER_FUEL.tag, calculatedCost);
+        }
+
+        List<FluidStack> fuels = new ArrayList<>(fuelFluid.getMatchingFluidStacks());
+
+        if (!fuels.isEmpty() && !fuels.get(0).isEmpty() && fuels.get(0) != null) builder
+                .addSlot(RecipeIngredientRole.INPUT, 36, 32)
+                .setBackground(getRenderedSlot(), -1, -1)
+                .addIngredients(ForgeTypes.FLUID_STACK, withImprovedVisibility(fuels))
+                .addTooltipCallback(addFluidTooltip(fuelFluid.getRequiredAmount()))
+                .addTooltipCallback((v, t) -> t.add(Component
+                        .translatable("blazinghot.tooltip.blaze_mixing.fuel")
+                        .withStyle(ChatFormatting.DARK_GREEN)));
+    }
+
+    @Override
+    public void draw(@NotNull BasinRecipe recipe, @NotNull IRecipeSlotsView iRecipeSlotsView, @NotNull GuiGraphics graphics, double mouseX, double mouseY) {
+        super.draw(recipe, iRecipeSlotsView, graphics, mouseX, mouseY);
+
+        HeatCondition requiredHeat = recipe.getRequiredHeat();
+        if (requiredHeat != HeatCondition.NONE) heater
+                .withHeat(requiredHeat.visualizeAsBlazeBurner())
+                .draw(graphics, getBackground().getWidth() / 2 + 3, 55);
+        mixer.draw(graphics, getBackground().getWidth() / 2 + 3, 34);
+    }
+}
---
+++ forge/src/main/java/com/dudko/blazinghot/compat/jei/forge/BlazingJEI.java
@@ -0,0 +1,326 @@
+package com.dudko.blazinghot.compat.jei.forge;
+
+import com.dudko.blazinghot.BlazingHot;
+import com.dudko.blazinghot.mixin.forge.RecipeManagerAccessor;
+import com.dudko.blazinghot.registry.BlazingBlocks;
+import com.dudko.blazinghot.registry.BlazingRecipeTypes;
+import com.dudko.blazinghot.registry.forge.BlazingRecipeTypesImpl;
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllRecipeTypes;
+import com.simibubi.create.compat.jei.DoubleItemIcon;
+import com.simibubi.create.compat.jei.EmptyBackground;
+import com.simibubi.create.compat.jei.ItemIcon;
+import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
+import com.simibubi.create.content.fluids.potion.PotionMixingRecipes;
+import com.simibubi.create.content.kinetics.press.MechanicalPressBlockEntity;
+import com.simibubi.create.content.processing.basin.BasinRecipe;
+import com.simibubi.create.foundation.config.ConfigBase;
+import com.simibubi.create.foundation.item.ItemHelper;
+import com.simibubi.create.foundation.recipe.IRecipeTypeInfo;
+import com.simibubi.create.infrastructure.config.AllConfigs;
+import com.simibubi.create.infrastructure.config.CRecipes;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.ShapedRecipe;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+@JeiPlugin
+@SuppressWarnings("SameParameterValue")
+public class BlazingJEI implements IModPlugin {
+
+    private static final ResourceLocation ID = BlazingHot.asResource("jei_plugin");
+
+    private final List<CreateRecipeCategory<?>> allCategories = new ArrayList<>();
+
+    @SuppressWarnings("unused")
+    private void loadCategories() {
+        allCategories.clear();
+
+        CreateRecipeCategory<?>
+                blazeMixing =
+                builder(BasinRecipe.class)
+                        .addTypedRecipes(BlazingRecipeTypesImpl.BLAZE_MIXING)
+                        .addTypedRecipesExcluding(AllRecipeTypes.MIXING::getType,
+                                BlazingRecipeTypes.BLAZE_MIXING::getType)
+                        .catalyst(BlazingBlocks.BLAZE_MIXER::get)
+                        .catalyst(AllBlocks.BASIN::get)
+                        .doubleItemIcon(BlazingBlocks.BLAZE_MIXER.get(), AllBlocks.BASIN.get())
+                        .emptyBackground(177, 103)
+                        .build("blaze_mixing", BlazeMixingCategory::standard),
+
+                blazeAutoShapeless =
+                        builder(BasinRecipe.class)
+                                .enableWhen(c -> c.allowShapelessInMixer)
+                                .addAllRecipesIf(r -> r instanceof CraftingRecipe
+                                        && !(r instanceof ShapedRecipe)
+                                        && r.getIngredients().size() > 1
+                                        && !MechanicalPressBlockEntity.canCompress(r)
+                                        && !AllRecipeTypes.shouldIgnoreInAutomation(r), BasinRecipe::convertShapeless)
+                                .catalyst(BlazingBlocks.BLAZE_MIXER::get)
+                                .catalyst(AllBlocks.BASIN::get)
+                                .doubleItemIcon(BlazingBlocks.BLAZE_MIXER.get(), Items.CRAFTING_TABLE)
+                                .emptyBackground(177, 103)
+                                .build("blaze_automatic_shapeless", BlazeMixingCategory::autoShapeless),
+
+                blazeBrewing =
+                        builder(BasinRecipe.class)
+                                .enableWhen(c -> c.allowBrewingInMixer)
+                                .addRecipes(() -> PotionMixingRecipes.ALL)
+                                .catalyst(BlazingBlocks.BLAZE_MIXER::get)
+                                .catalyst(AllBlocks.BASIN::get)
+                                .doubleItemIcon(BlazingBlocks.BLAZE_MIXER.get(), Blocks.BREWING_STAND)
+                                .emptyBackground(177, 103)
+                                .build("blaze_automatic_brewing", BlazeMixingCategory::autoBrewing);
+    }
+
+    private <T extends Recipe<?>> CategoryBuilder<T> builder(Class<? extends T> recipeClass) {
+        return new CategoryBuilder<>(recipeClass);
+    }
+
+    @Override
+    @Nonnull
+    public ResourceLocation getPluginUid() {
+        return ID;
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        loadCategories();
+        registration.addRecipeCategories(allCategories.toArray(IRecipeCategory[]::new));
+    }
+
+    @Override
+    public void registerRecipes(@NotNull IRecipeRegistration registration) {
+        allCategories.forEach(c -> c.registerRecipes(registration));
+    }
+
+    @Override
+    public void registerRecipeCatalysts(@NotNull IRecipeCatalystRegistration registration) {
+        allCategories.forEach(c -> c.registerCatalysts(registration));
+    }
+
+    private class CategoryBuilder<T extends Recipe<?>> {
+        private final Class<? extends T> recipeClass;
+        private Predicate<CRecipes> predicate = cRecipes -> true;
+
+        private IDrawable background;
+        private IDrawable icon;
+
+        private final List<Consumer<List<T>>> recipeListConsumers = new ArrayList<>();
+        private final List<Supplier<? extends ItemStack>> catalysts = new ArrayList<>();
+
+        public CategoryBuilder(Class<? extends T> recipeClass) {
+            this.recipeClass = recipeClass;
+        }
+
+        public CategoryBuilder<T> enableIf(Predicate<CRecipes> predicate) {
+            this.predicate = predicate;
+            return this;
+        }
+
+        public CategoryBuilder<T> enableWhen(Function<CRecipes, ConfigBase.ConfigBool> configValue) {
+            predicate = c -> configValue.apply(c).get();
+            return this;
+        }
+
+        public CategoryBuilder<T> addRecipeListConsumer(Consumer<List<T>> consumer) {
+            recipeListConsumers.add(consumer);
+            return this;
+        }
+
+        public CategoryBuilder<T> addRecipes(Supplier<Collection<? extends T>> collection) {
+            return addRecipeListConsumer(recipes -> recipes.addAll(collection.get()));
+        }
+
+        public CategoryBuilder<T> addAllRecipesIf(Predicate<Recipe<?>> pred) {
+            return addRecipeListConsumer(recipes -> consumeAllRecipes(recipe -> {
+                if (pred.test(recipe)) {
+                    recipes.add((T) recipe);
+                }
+            }));
+        }
+
+        public CategoryBuilder<T> addAllRecipesIf(Predicate<Recipe<?>> pred, Function<Recipe<?>, T> converter) {
+            return addRecipeListConsumer(recipes -> consumeAllRecipes(recipe -> {
+                if (pred.test(recipe)) {
+                    recipes.add(converter.apply(recipe));
+                }
+            }));
+        }
+
+        public CategoryBuilder<T> addTypedRecipes(IRecipeTypeInfo recipeTypeEntry) {
+            return addTypedRecipes(recipeTypeEntry::getType);
+        }
+
+        public CategoryBuilder<T> addTypedRecipes(Supplier<RecipeType<? extends T>> recipeType) {
+            return addRecipeListConsumer(recipes -> BlazingJEI.<T>consumeTypedRecipes(recipes::add, recipeType.get()));
+        }
+
+        public CategoryBuilder<T> addTypedRecipes(Supplier<RecipeType<? extends T>> recipeType, Function<Recipe<?>, T> converter) {
+            return addRecipeListConsumer(recipes -> BlazingJEI.<T>consumeTypedRecipes(recipe -> recipes.add(converter.apply(
+                    recipe)), recipeType.get()));
+        }
+
+        public CategoryBuilder<T> addTypedRecipesIf(Supplier<RecipeType<? extends T>> recipeType, Predicate<Recipe<?>> pred) {
+            return addRecipeListConsumer(recipes -> BlazingJEI.<T>consumeTypedRecipes(recipe -> {
+                if (pred.test(recipe)) {
+                    recipes.add(recipe);
+                }
+            }, recipeType.get()));
+        }
+
+        public CategoryBuilder<T> addTypedRecipesExcluding(Supplier<RecipeType<? extends T>> recipeType, Supplier<RecipeType<? extends T>> excluded) {
+            return addRecipeListConsumer(recipes -> {
+                List<Recipe<?>> excludedRecipes = getTypedRecipes(excluded.get());
+                BlazingJEI.<T>consumeTypedRecipes(recipe -> {
+                    for (Recipe<?> excludedRecipe : excludedRecipes) {
+                        if (doInputsMatch(recipe, excludedRecipe)) {
+                            return;
+                        }
+                    }
+                    recipes.add(recipe);
+                }, recipeType.get());
+            });
+        }
+
+        public CategoryBuilder<T> removeRecipes(Supplier<RecipeType<? extends T>> recipeType) {
+            return addRecipeListConsumer(recipes -> {
+                List<Recipe<?>> excludedRecipes = getTypedRecipes(recipeType.get());
+                recipes.removeIf(recipe -> {
+                    for (Recipe<?> excludedRecipe : excludedRecipes)
+                        if (doInputsMatch(recipe, excludedRecipe) && doOutputsMatch(recipe, excludedRecipe))
+                            return true;
+                    return false;
+                });
+            });
+        }
+
+        public CategoryBuilder<T> catalystStack(Supplier<ItemStack> supplier) {
+            catalysts.add(supplier);
+            return this;
+        }
+
+        public CategoryBuilder<T> catalyst(Supplier<ItemLike> supplier) {
+            return catalystStack(() -> new ItemStack(supplier.get().asItem()));
+        }
+
+        public CategoryBuilder<T> icon(IDrawable icon) {
+            this.icon = icon;
+            return this;
+        }
+
+        public CategoryBuilder<T> itemIcon(ItemLike item) {
+            icon(new ItemIcon(() -> new ItemStack(item)));
+            return this;
+        }
+
+        public CategoryBuilder<T> doubleItemIcon(ItemLike item1, ItemLike item2) {
+            icon(new DoubleItemIcon(() -> new ItemStack(item1), () -> new ItemStack(item2)));
+            return this;
+        }
+
+        public CategoryBuilder<T> background(IDrawable background) {
+            this.background = background;
+            return this;
+        }
+
+        public CategoryBuilder<T> emptyBackground(int width, int height) {
+            background(new EmptyBackground(width, height));
+            return this;
+        }
+
+        public CreateRecipeCategory<T> build(String name, CreateRecipeCategory.Factory<T> factory) {
+            Supplier<List<T>> recipesSupplier;
+            if (predicate.test(AllConfigs.server().recipes)) {
+                recipesSupplier = () -> {
+                    List<T> recipes = new ArrayList<>();
+                    for (Consumer<List<T>> consumer : recipeListConsumers)
+                        consumer.accept(recipes);
+                    return recipes;
+                };
+            }
+            else {
+                recipesSupplier = Collections::emptyList;
+            }
+
+            CreateRecipeCategory.Info<T>
+                    info =
+                    new CreateRecipeCategory.Info<>(new mezz.jei.api.recipe.RecipeType<>(BlazingHot.asResource(name),
+                            recipeClass),
+                            Component.translatable(BlazingHot.ID + ".recipe." + name),
+                            background,
+                            icon,
+                            recipesSupplier,
+                            catalysts);
+            CreateRecipeCategory<T> category = factory.create(info);
+            allCategories.add(category);
+            return category;
+        }
+    }
+
+    public static void consumeAllRecipes(Consumer<Recipe<?>> consumer) {
+        Minecraft.getInstance().getConnection().getRecipeManager().getRecipes().forEach(consumer);
+    }
+
+    public static <T extends Recipe<?>> void consumeTypedRecipes(Consumer<T> consumer, RecipeType<?> type) {
+        Map<ResourceLocation, Recipe<?>>
+                map =
+                ((RecipeManagerAccessor) Minecraft.getInstance().getConnection().getRecipeManager())
+                        .getRecipes()
+                        .get(type);
+        if (map != null) {
+            map.values().forEach(recipe -> consumer.accept((T) recipe));
+        }
+    }
+
+    public static List<Recipe<?>> getTypedRecipes(RecipeType<?> type) {
+        List<Recipe<?>> recipes = new ArrayList<>();
+        consumeTypedRecipes(recipes::add, type);
+        return recipes;
+    }
+
+    public static List<Recipe<?>> getTypedRecipesExcluding(RecipeType<?> type, Predicate<Recipe<?>> exclusionPred) {
+        List<Recipe<?>> recipes = getTypedRecipes(type);
+        recipes.removeIf(exclusionPred);
+        return recipes;
+    }
+
+    public static boolean doInputsMatch(Recipe<?> recipe1, Recipe<?> recipe2) {
+        if (recipe1.getIngredients().isEmpty() || recipe2.getIngredients().isEmpty()) {
+            return false;
+        }
+        ItemStack[] matchingStacks = recipe1.getIngredients().get(0).getItems();
+        if (matchingStacks.length == 0) {
+            return false;
+        }
+        return recipe2.getIngredients().get(0).test(matchingStacks[0]);
+    }
+
+    public static boolean doOutputsMatch(Recipe<?> recipe1, Recipe<?> recipe2) {
+        RegistryAccess registryAccess = Minecraft.getInstance().level.registryAccess();
+        return ItemHelper.sameItem(recipe1.getResultItem(registryAccess), recipe2.getResultItem(registryAccess));
+    }
+}
