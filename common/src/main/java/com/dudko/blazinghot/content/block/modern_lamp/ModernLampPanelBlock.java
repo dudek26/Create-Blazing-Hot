42c1a169fecde94f996d2c104dafef17be8b855b
dudek26 <f.j.kowalik@gmail.com>
2024-08-14 15:02:28 +0200

    waterloggable modern lamp panels

--- common/src/main/java/com/dudko/blazinghot/content/block/modern_lamp/ModernLampPanelBlock.java
+++ common/src/main/java/com/dudko/blazinghot/content/block/modern_lamp/ModernLampPanelBlock.java
@@ -6,35 +6,58 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
+import static net.minecraft.world.level.block.state.properties.BlockStateProperties.WATERLOGGED;
+
 @SuppressWarnings("deprecation")
-public class ModernLampPanelBlock extends ModernLampBlock {
+public class ModernLampPanelBlock extends ModernLampBlock implements SimpleWaterloggedBlock {
 
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
     public ModernLampPanelBlock(Properties properties, DyeColor color) {
         super(properties, color);
-        defaultBlockState().setValue(FACING, Direction.UP);
+        defaultBlockState().setValue(FACING, Direction.UP).setValue(WATERLOGGED, false);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        super.createBlockStateDefinition(pBuilder.add(FACING));
+        super.createBlockStateDefinition(pBuilder.add(FACING, WATERLOGGED));
     }
 
     @Override
     public BlockState getStateForPlacement(@NotNull BlockPlaceContext pContext) {
         BlockState stateForPlacement = super.getStateForPlacement(pContext);
-        return stateForPlacement.setValue(FACING, pContext.getClickedFace());
+        assert stateForPlacement != null;
+        return stateForPlacement.setValue(FACING, pContext.getClickedFace())
+                .setValue(WATERLOGGED, pContext.getLevel()
+                        .getFluidState(pContext.getClickedPos())
+                        .getType() == Fluids.WATER);
+    }
+
+    @Override
+    public @NotNull FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : Fluids.EMPTY.defaultFluidState();
+    }
+
+    @Override
+    public @NotNull BlockState updateShape(BlockState state, @NotNull Direction direction, @NotNull BlockState neighbourState, @NotNull LevelAccessor world,
+                                           @NotNull BlockPos pos, @NotNull BlockPos neighbourPos) {
+        if (state.getValue(WATERLOGGED))
+            world.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
+        return state;
     }
 
     @Override
