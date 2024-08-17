1f9c9422a43977262a7d89d7a5cadf30ae6870c8
dudko <45921058+dudek26@users.noreply.github.com>
2024-08-18 00:25:07 +0200

    Update BlazingAdvancements.java

--- fabric/src/main/java/com/dudko/blazinghot/data/advancement/fabric/BlazingAdvancements.java
+++ fabric/src/main/java/com/dudko/blazinghot/data/advancement/fabric/BlazingAdvancements.java
@@ -28,8 +28,8 @@ public class BlazingAdvancements implements DataProvider {
     public static final BlazingAdvancement START = null,
 
     ROOT = create("root", b -> b.icon(BlazingItems.BLAZE_GOLD_INGOT)
-            .title("Create: Hell Edition")
-            .description("Don't burn yourself!")
+            .title("Blazing Hot")
+            .description("Create: Hell Edition")
             .awardedForFree()
             .special(SILENT)),
 
