27091d5c1412a0d677163f34be04e6022cffd3c7
dudko <f.j.kowalik@gmail.com>
2024-09-28 15:56:53 +0200

    wip casting depot

--- common/src/main/java/com/dudko/blazinghot/content/processing/casting_depot/CastingDepotBlockEntity.java
+++ common/src/main/java/com/dudko/blazinghot/content/processing/casting_depot/CastingDepotBlockEntity.java
@@ -1,12 +1,16 @@
 package com.dudko.blazinghot.content.processing.casting_depot;
 
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import com.simibubi.create.foundation.blockEntity.behaviour.fluid.SmartFluidTankBehaviour;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public abstract class CastingDepotBlockEntity extends SmartBlockEntity {
+
+	protected SmartFluidTankBehaviour tank;
+
 	public CastingDepotBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
 		super(type, pos, state);
 	}
