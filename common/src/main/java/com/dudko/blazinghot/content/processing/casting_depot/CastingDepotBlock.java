a32c7e9a333c73dc838cfe705c23b69245f8dd3e
dudko <f.j.kowalik@gmail.com>
2024-09-29 00:38:10 +0200

    wip casting depot, new advancements

--- common/src/main/java/com/dudko/blazinghot/content/processing/casting_depot/CastingDepotBlock.java
+++ common/src/main/java/com/dudko/blazinghot/content/processing/casting_depot/CastingDepotBlock.java
@@ -5,17 +5,23 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import com.dudko.blazinghot.content.block.shape.Shapes;
 import com.simibubi.create.AllShapes;
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
+import com.simibubi.create.content.logistics.depot.SharedDepotBlockMethods;
 import com.simibubi.create.foundation.block.IBE;
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -50,5 +56,20 @@ public class CastingDepotBlock extends Block implements IWrenchable, IBE<Casting
 	@ExpectPlatform
 	public static <T extends CastingDepotBlockEntity> BlockEntityType<T> platformedBlockEntity() {
 		throw new AssertionError();
+	}
+
+	@Override
+	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult ray) {
+		return SharedDepotBlockMethods.onUse(state, world, pos, player, hand, ray);
+	}
+
+	@Override
+	public void onRemove(BlockState state, Level worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+		IBE.onRemove(state, worldIn, pos, newState);
+	}
+
+	@Override
+	public boolean isPathfindable(BlockState state, BlockGetter reader, BlockPos pos, PathComputationType type) {
+		return false;
 	}
 }
