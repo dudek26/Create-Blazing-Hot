3cb38031d7280314975443db3d25bc4c82544255
dudko <f.j.kowalik@gmail.com>
2024-10-05 00:39:10 +0200

    wip casting depot

--- forge/src/main/java/com/dudko/blazinghot/content/casting/casting_depot/forge/CastingDepotBlockEntityImpl.java
+++ forge/src/main/java/com/dudko/blazinghot/content/casting/casting_depot/forge/CastingDepotBlockEntityImpl.java
@@ -6,12 +6,38 @@ import com.dudko.blazinghot.content.casting.casting_depot.CastingDepotBlockEntit
 import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class CastingDepotBlockEntityImpl extends CastingDepotBlockEntity {
 	public CastingDepotBlockEntityImpl(BlockEntityType<?> type, BlockPos pos, BlockState state) {
 		super(type, pos, state);
+	}
+
+	@Override
+	public void onMoldUpdate() {
+
+	}
+
+	@Override
+	public void updateCapacity(long newCapacity) {
+
+	}
+
+	@Override
+	public long getCapacity() {
+		return 0;
+	}
+
+	@Override
+	public ItemStack getHeldItem() {
+		return null;
+	}
+
+	@Override
+	public long getFluidAmount() {
+		return 0;
 	}
 
 	@Override
