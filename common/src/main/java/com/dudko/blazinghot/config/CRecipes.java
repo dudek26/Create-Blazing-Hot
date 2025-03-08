1a2b26efd96270059e6306ca2cc0001949a233bd
dudko <f.j.kowalik@gmail.com>
2025-03-08 21:27:31 +0100

    fix recipes config

--- common/src/main/java/com/dudko/blazinghot/config/CRecipes.java
+++ common/src/main/java/com/dudko/blazinghot/config/CRecipes.java
@@ -1,7 +1,7 @@
 package com.dudko.blazinghot.config;
 
-import com.dudko.blazinghot.multiloader.MultiFluids;
-
+import com.dudko.blazinghot.multiloader.fluid.MultiAmount;
+import com.dudko.blazinghot.multiloader.fluid.MultiFluids;
 import net.createmod.catnip.config.ConfigBase;
 
 public class CRecipes extends ConfigBase {
@@ -10,7 +10,7 @@ public class CRecipes extends ConfigBase {
 
 	}
 
-	private static final int DEFAULT_FUEL_USAGE = (int) MultiFluids.fromBucketFraction(1, 40);
+	private static final int DEFAULT_FUEL_USAGE = (int) MultiAmount.fromBucketFraction(1, 40).get();
 
 	public final ConfigBool
 			allowMixingInBlazeMixer =
