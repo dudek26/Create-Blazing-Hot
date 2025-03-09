3ee046451c4882b5b1a1a87872878260a7ad5036
dudko <f.j.kowalik@gmail.com>
2025-03-09 11:22:10 +0100

    run datagen

--- common/src/main/java/com/dudko/blazinghot/mixin/accessor/SequencedAssemblyRecipeBuilderAccessor.java
+++ common/src/main/java/com/dudko/blazinghot/mixin/accessor/SequencedAssemblyRecipeBuilderAccessor.java
@@ -1,16 +1,14 @@
 package com.dudko.blazinghot.mixin.accessor;
 
-import com.simibubi.create.content.processing.recipe.ProcessingRecipe;
 import com.simibubi.create.content.processing.sequenced.SequencedAssemblyRecipe;
 import com.simibubi.create.content.processing.sequenced.SequencedAssemblyRecipeBuilder;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(SequencedAssemblyRecipeBuilder.class)
 public interface SequencedAssemblyRecipeBuilderAccessor {
 
-	@Accessor
-	SequencedAssemblyRecipe getRecipe();
+    @Accessor(remap = false)
+    SequencedAssemblyRecipe getRecipe();
 
 }
