eb450e87498aad76dd07f0f156744557052baa31
dudko <f.j.kowalik@gmail.com>
2025-03-09 12:21:07 +0100

    fix fabric mixin

--- fabric/src/main/java/com/dudko/blazinghot/multiloader/fluid/fabric/MultiFluidStackImpl.java
+++ fabric/src/main/java/com/dudko/blazinghot/multiloader/fluid/fabric/MultiFluidStackImpl.java
@@ -4,14 +4,10 @@ import com.dudko.blazinghot.multiloader.fluid.MultiAmount;
 import com.dudko.blazinghot.multiloader.fluid.MultiFluidStack;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
 import com.google.gson.JsonSyntaxException;
-
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.simibubi.create.Create;
 
-import io.github.fabricators_of_create.porting_lib.fluids.FluidStack;
-import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.TagParser;
@@ -25,19 +21,19 @@ public class MultiFluidStackImpl {
 	public static MultiFluidStack deserializeFluidStack(JsonObject json) {
 		ResourceLocation id = new ResourceLocation(GsonHelper.getAsString(json, "fluid"));
 		Fluid fluid = BuiltInRegistries.FLUID.get(id);
-		if (fluid == null)
-			throw new JsonSyntaxException("Unknown fluid '" + id + "'");
-		if (fluid == Fluids.EMPTY)
-			throw new JsonSyntaxException("Invalid empty fluid '" + id + "'");
+		if (fluid == null) throw new JsonSyntaxException("Unknown fluid '" + id + "'");
+		if (fluid == Fluids.EMPTY) throw new JsonSyntaxException("Invalid empty fluid '" + id + "'");
 		long amount = GsonHelper.getAsLong(json, "amount");
 		MultiFluidStack stack = new MultiFluidStack(fluid, MultiAmount.standard(amount));
-		if (!json.has("nbt"))
-			return stack;
+		if (!json.has("nbt")) return stack;
 
 		try {
 			JsonElement element = json.get("nbt");
-			CompoundTag nbt = TagParser.parseTag(
-					element.isJsonObject() ? Create.GSON.toJson(element) : GsonHelper.convertToString(element, "nbt"));
+			CompoundTag
+					nbt =
+					TagParser.parseTag(element.isJsonObject() ?
+									   Create.GSON.toJson(element) :
+									   GsonHelper.convertToString(element, "nbt"));
 			stack.setTag(nbt);
 		} catch (CommandSyntaxException e) {
 			throw new JsonSyntaxException("Failed to read NBT", e);
