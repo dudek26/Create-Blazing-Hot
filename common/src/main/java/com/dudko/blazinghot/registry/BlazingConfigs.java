10d9cfc348be47fb63437968f7ec7c9014b4b154
dudko <45921058+dudek26@users.noreply.github.com>
2025-11-10 20:31:51 +0100

    fix server crash

--- common/src/main/java/com/dudko/blazinghot/registry/BlazingConfigs.java
+++ common/src/main/java/com/dudko/blazinghot/registry/BlazingConfigs.java
@@ -5,6 +5,8 @@ import com.dudko.blazinghot.config.CClient;
 import com.dudko.blazinghot.config.CServer;
 
 import net.createmod.catnip.config.ui.BaseConfigScreen;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screens.Screen;
 
 public class BlazingConfigs {
@@ -19,7 +21,8 @@ public class BlazingConfigs {
 	public static CServer server() {
 		return server;
 	}
-	
+
+	@Environment(EnvType.CLIENT)
 	public static BaseConfigScreen createConfigScreen(Screen parent) {
 		BaseConfigScreen.setDefaultActionFor(BlazingHot.ID,
 				(base) -> base
