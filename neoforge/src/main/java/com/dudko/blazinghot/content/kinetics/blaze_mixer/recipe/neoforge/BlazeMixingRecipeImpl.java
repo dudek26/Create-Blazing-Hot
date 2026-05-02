02703b9d30bc5ebb62625342e500332f92ee21e4
dudko <45921058+dudek26@users.noreply.github.com>
2026-05-02 23:50:56 +0300

    blaze mixing rework IV - wip inferno blaze mixer

--- neoforge/src/main/java/com/dudko/blazinghot/content/kinetics/blaze_mixer/recipe/neoforge/BlazeMixingRecipeImpl.java
+++ neoforge/src/main/java/com/dudko/blazinghot/content/kinetics/blaze_mixer/recipe/neoforge/BlazeMixingRecipeImpl.java
@@ -2,8 +2,8 @@ package com.dudko.blazinghot.content.kinetics.blaze_mixer.recipe.neoforge;
 
 import net.neoforged.neoforge.fluids.crafting.SizedFluidIngredient;
 
-public class AbstractBlazeMixingRecipeImpl {
-	
+public class BlazeMixingRecipeImpl {
+
 	public static boolean isPlaceholder(SizedFluidIngredient fluidIngredient) {
 		return fluidIngredient.getFluids().length == 0;
 	}
